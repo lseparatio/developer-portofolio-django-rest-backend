6301099b1680a600c60e0080fced938e5c3e2c95
lseparatio <consultantaanglia@gmail.com>
2022-11-27 03:37:34 +0000

    Added rest autentification

--- developer_portofolio_backend/urls.py
+++ developer_portofolio_backend/urls.py
@@ -22,6 +22,8 @@ urlpatterns = [
     # Admin Url's
     path('admin/', admin.site.urls),
     # All-Auth Url's
+    path('dj-rest-auth/', include('dj_rest_auth.urls')),
+    path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
     path('accounts/', include('allauth.urls')),
     # Profiles Url's
     path('api/', include('profiles.urls')),
