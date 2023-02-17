04318948c74f9d32b4913a9817d82bec3daadaa7
Ionut Zapototchi <67255838+lseparatio@users.noreply.github.com>
2023-02-17 13:52:19 +0000

    Activated old password field for password changes

--- developer_portofolio_backend/settings.py
+++ developer_portofolio_backend/settings.py
@@ -30,6 +30,7 @@ JWT_AUTH_SECURE = True
 JWT_AUTH_COOKIE = 'my-app-auth'
 JWT_AUTH_REFRESH_COOKIE = 'my-refresh-token'
 JWT_AUTH_SAMESITE = 'None'
+OLD_PASSWORD_FIELD_ENABLED = True
 
 REST_AUTH_SERIALIZERS = {
     'USER_DETAILS_SERIALIZER': 'developer_portofolio_backend.serializers.CurrentUserSerializer'
