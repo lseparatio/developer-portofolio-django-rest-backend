99c0046ab60d4e5a33ae68a940eda4e5b6046391
lseparatio <consultantaanglia@gmail.com>
2023-03-22 14:42:50 +0000

    Added 2 functions to send emails one back to form submiter and one to admin

--- contact/views.py
+++ contact/views.py
@@ -6,6 +6,10 @@ from rest_framework.response import Response
 from .models import Contact
 from .serializers import ContactSerializer
 from developer_portofolio_backend.permissions import IsUserOrReadOnly
+from django.core.mail import send_mail
+# To do:
+#  1, To allow get method only for admins
+#  2, To actualy send custom  html mails
 
 
 class ContactList(generics.ListCreateAPIView):
@@ -13,30 +17,30 @@ class ContactList(generics.ListCreateAPIView):
     queryset = Contact.objects.all()
 
     def perform_create(self, serializer):
+        name = serializer.validated_data['full_name']
+        email = serializer.validated_data['email']
+        sent_message = serializer.validated_data['sent_message']
+        send_message(name, email, sent_message)
+        send_confirmation(name, email, sent_message)
+
         serializer.save()
 
 
-class ProfileDetail(APIView):
-    serializer_class = ContactSerializer
-    permission_classes = [IsUserOrReadOnly]
+def send_message(name, email, sent_message):
+    send_mail(
+        email,
+        sent_message,
+        'contact@ionutzapototchi.com',
+        ['test@ionutzapototchi.com'],
+        fail_silently=False,
+    )
 
-    def get_object(self, pk):
-        try:
-            contact = Contact.objects.get(pk=pk)
-            self.check_object_permissions(self.request, contact)
-            return contact
-        except Contact.DoesNotExist:
-            raise Http404
 
-    def get(self, request, pk):
-        contact = self.get_object(pk)
-        serializer = ContactSerializer(
-            contact, context={'request': request}
-        )
-        return Response(serializer.data)
-
-    def put(self, request, pk):
-        contact = self.get_object(pk)
-        serializer = ContactSerializer(
-            contact, data=request.data, context={'request': request}
-        )
+def send_confirmation(name, email, sent_message):
+    send_mail(
+        'We got your message',
+        'We got your message and we will reply soon',
+        'contact@ionutzapototchi.com',
+        [email],
+        fail_silently=False,
+    )
