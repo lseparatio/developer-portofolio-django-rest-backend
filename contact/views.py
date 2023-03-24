330dbad3b136ee53dbfa8da40860aabe4e14c9a7
lseparatio <consultantaanglia@gmail.com>
2023-03-24 04:58:47 +0000

    Nice email templates added, email image filles added in media folder

--- contact/views.py
+++ contact/views.py
@@ -8,10 +8,10 @@ from .serializers import ContactSerializer
 from developer_portofolio_backend.permissions import IsUserOrReadOnly
 from django.conf import settings
 from django.template.loader import render_to_string
+from django.core.mail import EmailMultiAlternatives
 from django.core.mail import send_mail
 # To do:
 #  1, To allow get method only for admins
-#  2, Mail templates added, left to build nice email templates.
 
 
 class ContactList(generics.ListCreateAPIView):
@@ -22,29 +22,29 @@ class ContactList(generics.ListCreateAPIView):
         name = serializer.validated_data['full_name']
         email = serializer.validated_data['email']
         sent_message = serializer.validated_data['sent_message']
+        serializer.save()
         send_contact_email_to_us(name, email, sent_message)
         send_contact_confirmation_email(name, email, sent_message)
-        send_contact_confirmation_email(name, email, sent_message)
-
-        serializer.save()
 
 
 def send_contact_email_to_us(name, email, sent_message):
     """Send the content of contact form submision to admin"""
-    cust_email = email
+    email = email
     subject = render_to_string(
         'contact/confirmation_emails/send_contact_email_to_us_subject.txt',
         {'name': name})
-    body = render_to_string(
+    html_body = render_to_string(
         'contact/confirmation_emails/send_contact_email_to_us_body.html',
         {'name': name, 'email': email, 'sent_message': sent_message, 'contact_email': settings.DEFAULT_FROM_EMAIL})
 
-    send_mail(
-        subject,
-        body,
-        settings.DEFAULT_FROM_EMAIL,
-        [settings.DEFAULT_FROM_EMAIL]
+    message = EmailMultiAlternatives(
+        subject=subject,
+        body='',
+        from_email=settings.DEFAULT_FROM_EMAIL,
+        to=[settings.DEFAULT_FROM_EMAIL]
     )
+    message.attach_alternative(html_body, "text/html")
+    message.send(fail_silently=False)
 
 
 def send_contact_confirmation_email(name, email, sent_message):
@@ -53,13 +53,15 @@ def send_contact_confirmation_email(name, email, sent_message):
     subject = render_to_string(
         'contact/confirmation_emails/send_contact_confirmation_email_subject.txt',
         {'name': name})
-    body = render_to_string(
+    html_body = render_to_string(
         'contact/confirmation_emails/send_contact_confirmation_email_body.html',
         {'name': name, 'email': email, 'sent_message': sent_message, 'contact_email': settings.DEFAULT_FROM_EMAIL})
 
-    send_mail(
-        subject,
-        body,
-        settings.DEFAULT_FROM_EMAIL,
-        [cust_email]
+    message = EmailMultiAlternatives(
+        subject=subject,
+        body='',
+        from_email=settings.DEFAULT_FROM_EMAIL,
+        to=[cust_email]
     )
+    message.attach_alternative(html_body, "text/html")
+    message.send(fail_silently=False)
